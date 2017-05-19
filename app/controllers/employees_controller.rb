3f4c0ff02a51b51852ea7582faf1caaa22a14cab
Charlie Hsieh <charlieofficial@gmail.com>
2017-05-19 13:36:46 +0800

    tested and notes about , form_for, form_tag, link_to, button_to

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -6,6 +6,24 @@ class EmployeesController < ApplicationController
   end
 
   def search
-    @my_val = params[:q]
+    #render plain: @my_val.inspect
+    #@employee = Employee.new(name: @my_val)
+
+    @employee = Employee.new(employee_params)
+
+    respond_to do |format|
+      if @employee.save
+        format.html { redirect_to employees_index_url, notice: 'Student was successfully created.' }
+        format.json { render :show, status: :created, location: @employee}
+      else
+        format.html { render :edit}
+        format.json { render json: @empolyee.errors, status: :unprocessable_entity }
+      end
+    end
   end
+
+  private
+    def employee_params
+      params.require(:employee).permit(:name)
+    end
 end
